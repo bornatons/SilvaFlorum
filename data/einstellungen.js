f068a6775242c521aa668af40a44e22529d58d51
bornatons <borna.tonsic2@gmail.com>
2026-09-07 09:56:54 +0200

    Update einstellungen.js

--- data/einstellungen.js
+++ data/einstellungen.js
@@ -21,23 +21,23 @@
 const EINSTELLUNGEN = {
 
   // Vor- und Nachname der Inhaberin (erscheint im Impressum und Datenschutz)
-  "name": "",
+  "name": "Silva Tonsic",
 
   // Adresse des Ateliers
-  "strasse": "",      // z.B. "Waldweg 3"
-  "plz_ort": "",      // z.B. "8000 Zürich"
+  "strasse": "Kilchbergstrasse 6a",      // z.B. "Waldweg 3"
+  "plz_ort": "8134 Adliswil",      // z.B. "8000 Zürich"
 
   // Kontakt
-  "email": "",        // z.B. "hallo@silvaflorum.ch"
-  "telefon": "",      // z.B. "079 123 45 67" oder "+41 79 123 45 67" (ohne «(0)»)
+  "email": "silvaflorum@gmail.com",        // z.B. "hallo@silvaflorum.ch"
+  "telefon": "079 769 23 33",      // z.B. "079 123 45 67" oder "+41 79 123 45 67" (ohne «(0)»)
 
   // Nur ausfüllen, falls das Geschäft im Handelsregister eingetragen ist
   // (z.B. "CHE-123.456.789"). Sonst einfach leer lassen.
-  "uid": "",
+  "uid": "CHE-234.034.860",
 
   // Formspree-ID für das Anfrageformular (siehe README, «Formular aktivieren»).
   // Zeigt Formspree die Adresse https://formspree.io/f/abcdwxyz an,
   // dann lautet die ID "abcdwxyz".
-  "formspree_id": ""
+  "formspree_id": "https://formspree.io/f/xwlkrkzw"
 
 };
